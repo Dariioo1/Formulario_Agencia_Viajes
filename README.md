687bf6df18818ee0f9f598f4f40ed42315d253fb
Dariioo1 <dariopaucar@outlook.es>
2021-07-28 17:37:46 +0200

    Formulario

---
+++ README.md
@@ -0,0 +1,1 @@
+Formulario Agencia de Viajes
---
+++ agencia.js
@@ -0,0 +1,47 @@
+
+let arrayJson =[];
+function datos()
+{
+let nombre = document.getElementById("Nombre").value;
+let apellidos = document.getElementById("Apellidos").value;
+let telefono = document.getElementById("Telefono").value;
+let email = document.getElementById("Email").value;
+let fechaSalida = document.getElementById("fechaSalida").value;
+let fechaVuelta = document.getElementById("fechaVuelta").value;
+let ciudades = document.getElementById("Ciudades").value;
+let numeroAdultos = document.getElementById("numero").value;
+let aerolinea = document.getElementById("aerolinea3").value;
+let informacion = document.getElementById("Comentario").value;
+let politicas = document.getElementById("Politicas").value;
+
+let Json =
+{
+    "Nombre":nombre, 
+    "Apellidos":apellidos ,
+    "Telefono":telefono,
+    "Email":email,
+    "fechaSalida":fechaSalida, 
+    "fechaVuelta":fechaVuelta ,
+    "Ciudades":ciudades,
+    "numero":numeroAdultos,
+    "aerolinea3":aerolinea,
+    "Comentario":informacion,
+    "Politicas":politicas
+}
+
+
+arrayJson.push(Json)
+console.log(arrayJson);
+
+
+// console.log(JSON.stringify(Json));
+
+// alert(JSON.stringify(Json));
+
+alert (JSON.stringify(arrayJson))
+}
+
+
+
+
+
